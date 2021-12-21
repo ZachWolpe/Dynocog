698fa708b9027a68d8e9dcf2904085b68628dabf
ZachWolpe <Zach.Wolpe@medibio.com.au>
2021-12-21 07:14:36 +0200

    .

--- Python Implementation/final instance/process data/dependencies.py
+++ Python Implementation/final instance/process data/dependencies.py
@@ -2,16 +2,33 @@
 
 # ========================================= Project Dependencies ========================================= #
 # visualization modules
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import plotly.express as px
+from plotly.subplots            import make_subplots
+import plotly.graph_objects     as go
+import matplotlib.pyplot        as plt
+import plotly.express           as px
 
 # statistical modules
-from scipy import stats
-import pandas as pd
-import numpy as np
+from sklearn.linear_model       import RidgeCV, SGDRegressor, BayesianRidge, ElasticNetCV, LarsCV, LassoCV, LassoLars, LassoLarsCV, LassoLarsIC, LinearRegression
+from sklearn.feature_selection  import mutual_info_classif, f_regression, mutual_info_regression, f_classif
+from sklearn.ensemble           import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection    import train_test_split, GridSearchCV
+from sklearn.base               import BaseEstimator, RegressorMixin
+from sklearn.preprocessing      import StandardScaler, OneHotEncoder
+from sklearn.metrics            import mean_squared_error, r2_score
+from sklearn.gaussian_process   import GaussianProcessRegressor
+from sklearn.pipeline           import make_pipeline, Pipeline
+from sklearn.compose            import ColumnTransformer
+from sklearn.impute             import SimpleImputer
+from sklearn.svm                import SVR
+from statsmodels.formula.api    import ols
+from mrmr                       import mrmr_regression
+from scipy                      import stats
+import statsmodels.api          as sm
+
+import pandas                   as pd
+import numpy                    as np
 import torch
+
 
 # software dev modules
 from tqdm import tqdm
@@ -31,3 +48,22 @@ plt.style.use('seaborn-darkgrid')
 warnings.filterwarnings('ignore')
 pd.options.display.max_rows = 4000
 # ========================================= Project Dependencies ========================================= #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
